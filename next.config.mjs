e89ee14ddd8b3fc851806084d456c9a2e29db7a4
priy-am <priyam2101753@gmail.com>
2025-08-20 16:12:05 +0530

    add domain name of image acceptance

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    domains: ['localhost', 'api.localhost.com'],
+    domains: ['job-search-backend-jsoa.onrender.com'],
   },
 };
 
